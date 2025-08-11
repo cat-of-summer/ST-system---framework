31a0e6624f19cdc362fceeb8c5ff0c013cc602ee
cat-of-summer <163355316+cat-of-summer@users.noreply.github.com>
2025-08-11 17:35:06 +0200

    .

--- ST_system/Autoloader.php
+++ ST_system/Autoloader.php
@@ -4,11 +4,33 @@ namespace ST_system;
 
 final class Autoloader {
 
-    public static function register(string $base_dir) {
+    private static $class_map = [];
+
+    public static function register_dir(string $base_dir) {
         if (trim($base_dir) == '')
             throw new \Exception("Некорректный путь к директории для автозагрузчика!");
             
         new self($base_dir);
+    }
+
+    public static function register_class(string $class_name, string $file_path) {
+        static $load_class_map_registered = false;
+
+        if ((strpos($file_path, '/') !== 0))
+            $file_path = $_SERVER['DOCUMENT_ROOT'].'/'.$file_path;
+
+        
+        if (!$load_class_map_registered) {
+            spl_autoload_register([self::class, 'load_class_map']);
+            $load_class_map_registered = true;
+        }
+        
+        self::$class_map[$class_name] = str_replace('//', '/', $file_path);
+    }
+
+    public static function load_class_map(string $class_name) {
+        if (isset(self::$class_map[$class_name]) && file_exists(self::$class_map[$class_name]))
+            require_once self::$class_map[$class_name];
     }
 
     private string $base_dir;
@@ -20,10 +42,10 @@ final class Autoloader {
 
         $this->base_dir = $base_dir;
 
-        spl_autoload_register([$this, 'load_class']);
+        spl_autoload_register([$this, 'load_dir']);
     }
 
-    public function load_class(string $class_name) {
+    public function load_dir(string $class_name) {
         $file = str_replace('//', '/', $this->base_dir.'/'.str_replace('\\', '/', $class_name).'.php');
 
         if (file_exists($file))
