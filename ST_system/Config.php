79914567a77e0b0885008501ce8202b3b6b8c7a6
cat-of-summer <163355316+cat-of-summer@users.noreply.github.com>
2025-12-15 09:46:05 +0200

    Добавить класс Config с методами инициализации и обработки событий

---
+++ ST_system/Config.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ST_system;
+
+class Config {
+
+    public static function init() {
+
+    }
+
+    public static function addEventListener() {
+        
+    }
+
+    public static function env() {
+
+    }
+
+    public static function config() {
+
+    }
+
+}
