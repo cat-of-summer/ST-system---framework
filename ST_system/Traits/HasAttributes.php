3b79c4ca3295650487be7e55dd77dc57bbc9b820
cat-of-summer <163355316+cat-of-summer@users.noreply.github.com>
2026-01-15 16:50:09 +0200

    Добавление трейта HasAttributes для управления атрибутами объекта

---
+++ ST_system/Traits/HasAttributes.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ST_system\Traits;
+
+trait HasAttributes {
+
+    // use HasAttributes {
+    //     __get as private getAttribute;
+    // }
+
+    protected array $attributes = [];
+
+    public function __get(string $name) {
+        return method_exists($this, $attribute = 'get'.ucfirst($name).'Attribute')
+            ? $this->{$attribute}()
+            : ($this->attributes[$name] ?? null);
+    }
+
+    public function __set(string $name, $value): void {
+        if (method_exists($this, $attribute = 'set'.ucfirst($name).'Attribute'))
+            $this->{$attribute}($value);
+        else
+            $this->attributes[$name] = $value;
+    }
+}
