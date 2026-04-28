55420be54eb684da8222398363f86189c4d8da0b
cat-of-summer <xramic39@mail.ru>
2026-04-28 09:56:52 +0200

    refactor: удалить дублирующийся код и улучшить структуру трейта HasConfig

--- ST_system/Traits/HasConfig.php
+++ ST_system/Traits/HasConfig.php
@@ -21,29 +21,6 @@ trait HasConfig {
         return Config::getImmutableConfig(static::class, $key);
     }
 
-<?php
-
-namespace ST_system\Traits;
-
-use ST_system\Config;
-use ST_system\Rule;
-
-trait HasConfig {
-
-    final public static function setConfig(array $config = []): void {
-        foreach ($config as $key => $value)
-            Config::setImmutableConfig(static::class, $key, $value);
-    }
-
-    final public static function config(string $key = '') {
-        static $initialized = [];
-        if (!isset($initialized[static::class])) {
-            Config::fillImmutableConfig(static::class, '', static::getDefaultConfig());
-            $initialized[static::class] = true;
-        }
-        return Config::getImmutableConfig(static::class, $key);
-    }
-
     public static function hasConfigInit(): void {
         static $initialized = null;
 
