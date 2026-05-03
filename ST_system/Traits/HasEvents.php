2f2ef77b2c04af7379fdef2238600927c4bf9fbc
cat-of-summer <xramic39@mail.ru>
2026-05-04 00:20:51 +0200

    fix

--- ST_system/Traits/HasEvents.php
+++ ST_system/Traits/HasEvents.php
@@ -14,17 +14,17 @@ trait HasEvents {
         $this->listeners[$event][] = $listener;
     }
 
-    private function fire(string $event, &...$params): void {
-        if (empty($this->listeners[$event])) return;
+    private function fire(string $event, &...$params) {
+        if (empty($this->listeners[$event])) return false;
 
         foreach ($this->listeners[$event] as $listener)
             call_user_func_array($listener, $params);
     }
 
-    final public function trigger(string $event, &...$params): void {
+    final public function trigger(string $event, &...$params) {
         if (in_array($event, static::getReservedEvents(), true))
             throw new \LogicException("Event '{$event}' is reserved and cannot be triggered externally.");
 
-        $this->fire($event, ...$params);
+        return $this->fire($event, ...$params);
     }
 }
