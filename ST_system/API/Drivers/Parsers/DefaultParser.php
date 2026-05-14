33173f16fc9ba22c8b1dd8c36af9f23e4d51f738
cat-of-summer <xramic39@mail.ru>
2026-05-14 13:38:29 +0200

    Изменен путь к кэшу в методе getDefaultConfig класса DefaultParser: удален символ '~' из директории кэша.

--- ST_system/API/Drivers/Parsers/DefaultParser.php
+++ ST_system/API/Drivers/Parsers/DefaultParser.php
@@ -20,7 +20,7 @@ class DefaultParser extends IntegrationDriver {
     protected static function getDefaultConfig(): array {
         return [
             'endpoint'          => '',
-            'cache'             => ['dir' => '~/cache/', 'ttl' => 3600, 'driver' => 'filesystem'],
+            'cache'             => ['dir' => '', 'ttl' => 3600, 'driver' => 'filesystem'],
             'headers' => [
                 'user-agent'                => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
                 'accept'                    => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
