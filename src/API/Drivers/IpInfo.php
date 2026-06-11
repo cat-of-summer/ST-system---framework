15b613172c5ce81e0761fcdd54cd76a354c956e9
cat-of-summer <xramic39@mail.ru>
2026-06-11 16:26:48 +0200

    Новый драйвер

---
+++ src/API/Drivers/IpInfo.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace ST_system\API\Drivers;
+
+use ST_system\API\IntegrationDriver;
+use ST_system\Rule;
+
+final class IpInfo extends IntegrationDriver {
+
+    protected static function getDefaultConfig(): array {
+        return [
+            'endpoint' => 'https://api.ipinfo.io/',
+            'cache' => [
+                'dir' => '~/cache/',
+                'ttl' => -1
+            ]
+        ];
+    }
+
+    private string $token;
+    private string $service = 'lite';
+
+    protected function __init(): void {
+
+        $this->on('__construct', function(string $token, string $service = 'lite') {
+            Rule::create('string|in:lite')->throwable()->check($service);
+
+            $this->token = $token;
+            $this->service = $service;
+        });
+
+        $this->on('build_url', function(&$request_url, $endpoint, $method, &$params) {
+            $params['token'] = $this->token;
+            $request_url = rtrim($this->getEndpoint(), '/') . '/' . $this->service . '/' . $params['ip'];
+            unset($params['ip']);
+        });
+
+        $this->registerMethodsMap([
+            'check' => [
+                'params' => [
+                    'ip' => 'string|required|default:me'
+                ]
+            ],
+        ]);
+
+    }
+
+}
